ebd5e89eadbd86ae42f81e742b87bf82a25cc746
sovicd <damir.sovic@quanti.cz>
2019-06-06 01:22:33 +0200

    Working version

--- app/src/main/java/cz/test/damirsovic/myqrobotcleaner/MainActivity.kt
+++ app/src/main/java/cz/test/damirsovic/myqrobotcleaner/MainActivity.kt
@@ -1,135 +1,150 @@
 package cz.test.damirsovic.myqrobotcleaner
 
-import android.content.res.Resources
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.support.annotation.UiThread
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import cz.test.damirsovic.myqrobotcleaner.robot.*
-import org.json.JSONObject
-import android.support.v4.view.MenuItemCompat.setShortcut
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.SubMenu
 import android.widget.TextView
+import cz.test.damirsovic.myqrobotcleaner.robot.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import org.json.JSONObject
 import java.io.File
-import java.io.IOException
-import java.net.URL
 import java.util.*
-import java.util.concurrent.Executors
-import kotlin.concurrent.thread
 
 
-class MainActivity() : AppCompatActivity(), RobotFinishedListener {
+class MainActivity : AppCompatActivity(), RobotFinishedListener {
 
-    val url = "https://https://myq-test.000webhostapp.com"
+    val url = "https://myq-test.000webhostapp.com"
     val testList = ArrayList<String>()
-    val client = OkHttpClient();
+    val client = OkHttpClient()
+    var jsonString: String? = null
+    var selectedName: String? = null
+    val PERMISSION_REQUEST_CODE = 10001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupPermissions()
         collectJson()
     }
 
-    override fun onResume() {
-        super.onResume()
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        super.onCreateOptionsMenu(menu)
-
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        menu.clear()
         var i = 0
         for (name in testList) {
             menu.add(0, i++, 0, name)
         }
-
-        return true
+        return super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        Executors.newSingleThreadExecutor().execute({
-//
-//
-//        }
-        when (item.getItemId()) {
-            0 -> {
-                val text = resources
-                    .openRawResource(R.raw.test1)
-                    .bufferedReader()
-                    .use { it.readText() }
-                runRobot(text)
-            }
-            1 -> {
-                val text = resources
-                    .openRawResource(R.raw.test2)
-                    .bufferedReader()
-                    .use { it.readText() }
-                runRobot(text)
-            }
-        }
+        selectedName = item.title.toString()
+        readJson(url + "/" + selectedName)
+
         return super.onOptionsItemSelected(item)
     }
 
-    private fun runRobot(text: String){
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("MainActivity", "Permission has been denied by user")
+                    return
+                } else {
+                    Log.i("MainActivity", "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    private fun runRobot(text: String) {
         val robot = MyQRobotCleaner(this, readFromJson(text))
         val robotThread = Thread(robot)
         robotThread.run()
     }
 
-    private fun readFromJson(text:String): JsonInputInfo {
+    private fun readFromJson(text: String): JsonInputInfo {
         val jsonObject = JSONObject(text)
         val jsonArray = jsonObject.getJSONArray("map")
-        val map = jsonArray.let {array1 -> 0.until(array1.length())
-                    .map { i -> array1.getJSONArray(i)
-                        .let{ array2 -> 0.until(array2.length())
-                                .map{ j -> if(array2.getString(j).equals("null")) null else array2.getString(j).first()}
+        val map = jsonArray.let { array1 ->
+            0.until(array1.length())
+                .map { i ->
+                    array1.getJSONArray(i)
+                        .let { array2 ->
+                            0.until(array2.length())
+                                .map { j -> if (array2.getString(j).equals("null")) null else array2.getString(j).first() }
                         }
-                    }
-            }
+                }
+        }
 
         val startInfo = jsonObject.getJSONObject("start")
         val x: Int = startInfo.getInt("X")
         val y: Int = startInfo.getInt("Y")
-        val orientation: Char = startInfo.getString("facing").first() as Char
+        val orientation: Char = startInfo.getString("facing").first()
         val commands: List<Movement> = jsonObject.getJSONArray("commands")
-            .let { 0.until(it.length())
-                .map { i -> Movement(it.optString(i)) } }
+            .let {
+                0.until(it.length())
+                    .map { i -> Movement(it.optString(i)) }
+            }
         val battery: Int = jsonObject.getInt("battery")
         return JsonInputInfo(map, JsonStartInfo(Position(x, y), orientation), commands, battery)
     }
 
     private fun collectJson() {
+        testList.clear()
         Log.d("Collect", "Collecting")
-        doAsync {
-            val content = readUrl(url)
-            uiThread {
-                Log.d("Collect", content)
-                testList.add("test1.json")
-                testList.add("test2.json")
-            }
-        }
+        testList.add("test1.json")
+        testList.add("test2.json")
+        invalidateOptionsMenu()
     }
 
-    fun readUrl(url: String): String? {
-        val request = Request.Builder()
-            .url(url)
-            .build();
 
-        val response = client.newCall(request).execute()
-        return response.body()?.string()
+    fun readJson(url: String) {
+        doAsync {
+            val request = Request.Builder()
+                .url(url)
+                .build()
+
+            val response = client.newCall(request).execute()
+            uiThread {
+                jsonString = response.body?.string()
+                runRobot(jsonString!!)
+            }
+
+        }
     }
 
     override fun onRobotFinished(info: JSONObject?) {
         val result = findViewById<TextView>(R.id.result)
         result.text = info.toString()
-        //File(fileName).writeText(fileContent)
+        // Save to file
+        val file = File(getExternalFilesDir(null).absolutePath, selectedName?.replace(".", "_result."))
+            .writeText(info.toString())
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("MainActivity", "Permission to record denied")
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
     }
 }
